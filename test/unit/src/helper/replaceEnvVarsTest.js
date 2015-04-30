db979e335020421e8208a3077c86d14613a6feda
Simon <simon@simonemms.com>
2015-04-30 20:20:38 +0100

    Added the unittests around the replace env vars helper function

---
+++ test/unit/src/helper/replaceEnvVarsTest.js
@@ -0,0 +1,64 @@
+/**
+ * replaceEnvVarsTest
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+var expect = require("chai").expect;
+var sinon = require("sinon");
+
+
+/* Files */
+var replaceEnvVars = require("../../../../src/helper/replaceEnvVars");
+
+
+describe("replaceEnvVars test", function () {
+
+    it("should ignore if no environment variables set", function () {
+
+        var obj = {
+            envvar1: "ENVVAR1",
+            child: {
+                envvar2: "ENVVAR2"
+            }
+        };
+
+        expect(replaceEnvVars(obj)).to.be.eql({
+            child: {}
+        });
+
+    });
+
+    it("should replace with the environment variable if present", function () {
+
+        var stub = sinon.sandbox.create().stub(process, "env", {
+            "ENVVAR1": "var1",
+            "ENVVAR2": "var2",
+            "ENVVAR3": "var3"
+        });
+
+        var obj = {
+            envvar1: "ENVVAR1",
+            envvar4: "ENVVAR4",
+            child: {
+                envvar2: "ENVVAR2"
+            }
+        };
+
+        expect(replaceEnvVars(obj)).to.be.eql({
+            envvar1: "var1",
+            child: {
+                envvar2: "var2"
+            }
+        });
+
+        stub.restore();
+
+    });
+
+});
