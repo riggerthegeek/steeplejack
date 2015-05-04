0a0d307b8603c37e984dd76df0aaa8c8886b2910
Simon <simon@simonemms.com>
2015-05-04 19:05:15 +0100

    Removed the test route/index file. It was causing travis errors in node 0.8.

--- test/unit/src/library/Routing.test.js
+++ test/unit/src/library/Routing.test.js
@@ -344,9 +344,6 @@ describe("Routing test", function () {
                 }, {
                     name: "route.js",
                     parent: path.join(process.cwd(), "/test/routes")
-                }, {
-                    name: "index.js",
-                    parent: path.join(process.cwd(), "/test/routes")
                 }]);
 
             });
