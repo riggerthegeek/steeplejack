47a51dbec0643c3a503b44e9638df3e570eaa79c
Simon Emms <simon@simonemms.com>
2015-12-12 11:18:55 +0000

    Created a combined typescript declaration file

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -14,11 +14,22 @@ var gulp = require("gulp");
 
 
 /* Files */
+var pkg = require("../../package.json");
 
 
 gulp.task("clean", [
+    "clean:declarations",
     "clean:ts"
 ]);
+
+
+gulp.task("clean:declarations", function () {
+
+    return del([
+        pkg.name + ".d.ts"
+    ]);
+
+});
 
 
 gulp.task("clean:ts", function () {
--- gulp/tasks/compile.js
+++ gulp/tasks/compile.js
@@ -9,15 +9,20 @@
 
 
 /* Third-party modules */
+var dts = require("dts-generator");
 var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var ts = require("gulp-typescript");
 
 
 /* Files */
+var pkg = require("../../package.json");
 
 
-gulp.task("compile", ["clean:ts"], function () {
+gulp.task("compile", [
+    "clean:ts",
+    "compile:declarations"
+], function () {
 
     var tsProject = ts.createProject("tsconfig.json");
 
@@ -30,5 +35,16 @@ gulp.task("compile", ["clean:ts"], function () {
     return tsResult.js
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("build"));
+
+});
+
+
+gulp.task("compile:declarations", ["clean:declarations"], function () {
+
+    dts.default({
+        name: pkg.name,
+        project: ".",
+        out: pkg.name + ".d.ts"
+    });
 
 });
