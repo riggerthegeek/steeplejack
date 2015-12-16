32fbd2697ef8236c45521c4fc608bc0931029bb6
Simon Emms <simon@simonemms.com>
2015-12-16 05:42:48 +0000

    Simplified the exporting of proxyquire and sinon

--- test/helpers/configure.ts
+++ test/helpers/configure.ts
@@ -8,8 +8,8 @@
 
 /* Third-party modules */
 import * as chai from "chai";
-import * as proxyquireModule from "proxyquire";
-import * as sinonModule from "sinon";
+import * as proxyquire from "proxyquire";
+import * as sinon from "sinon";
 import sinonChai = require("sinon-chai");
 
 
@@ -19,5 +19,5 @@ import sinonChai = require("sinon-chai");
 chai.use(sinonChai);
 
 export let expect = chai.expect;
-export let proxyquire = proxyquireModule;
-export let sinon = sinonModule;
+
+export {proxyquire, sinon};
