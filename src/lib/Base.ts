c103a95af1d769ea9d6e6ebbe966ed411e4bdfc5
Simon Emms <simon@simonemms.com>
2015-12-18 14:42:41 +0000

    Done

---
+++ src/lib/Base.ts
@@ -0,0 +1,27 @@
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+
+
+/**
+ * Base
+ *
+ * This is our Base object.  Everything extends from
+ * here to given consistency and allows for good
+ * inheritance across all flavours of JavaScript.
+ */
+export default class Base {
+
+
+    static extend (properties: {} = null, staticProperties: {} = null) {
+
+        return 2;
+
+    }
+
+
+}
---
+++ test/unit/src/lib/Base.test.ts
@@ -0,0 +1,32 @@
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+import Base from "../../../../src/lib/Base";
+import {expect} from "../../../helpers/configure";
+
+
+describe("Base library", () => {
+
+    //let Base: BaseRaw;
+    //beforeEach(() => {
+    //
+    //    Base = BaseRaw;
+    //    //Base = proxyquire("../../Base", {
+    //    //    "./dist/lib/Base": BaseRaw
+    //    //});
+    //
+    //});
+
+    describe("extension", () => {
+
+        describe("#es6 extension", () => {
+
+        });
+
+    });
+
+});
