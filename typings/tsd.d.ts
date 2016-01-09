7a7d04b516c9cbdd4846927887802d55583e7a27
Simon Emms <simon@simonemms.com>
2016-01-09 22:02:56 +0000

    Removed typings from the repo

--- typings/tsd.d.ts
+++
@@ -1,7 +0,0 @@
-
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
-/// <reference path="proxyquire/proxyquire.d.ts" />
-/// <reference path="sinon-chai/sinon-chai.d.ts" />
-/// <reference path="sinon/sinon.d.ts" />
-/// <reference path="node/node.d.ts" />
