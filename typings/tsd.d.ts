ebad8a81088bbfab0c5c33bcf4840c9e72ca4434
Simon Emms <simon@simonemms.com>
2016-01-10 16:31:36 +0000

    Added the typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="proxyquire/proxyquire.d.ts" />
+/// <reference path="sinon-chai/sinon-chai.d.ts" />
+/// <reference path="sinon/sinon.d.ts" />
