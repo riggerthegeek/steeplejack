f44f9f99a893476bf65653f05799146634225723
Simon Emms <simon@simonemms.com>
2016-01-11 10:14:10 +0000

    Added documentation

--- exception/fatal.ts
+++ exception/fatal.ts
@@ -17,6 +17,14 @@
 import {Exception} from "./";
 
 
+/**
+ * Fatal
+ *
+ * This is an error that cannot be recovered from. This
+ * is likely to be either when a datastore cannot respond
+ * or similar. Ultimately, this would return an HTTP 503
+ * error (or equivalent).
+ */
 export class FatalException extends Exception {
 
 
