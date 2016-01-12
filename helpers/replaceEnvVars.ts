e321c5d6b00ab93997c585c4a47dd9a427db0811
Simon Emms <simon@simonemms.com>
2016-01-12 12:00:12 +0000

    Replace environment variables helper

---
+++ helpers/replaceEnvVars.ts
@@ -0,0 +1,50 @@
+/**
+ * Replace Env Vars
+ *
+ * Looks for a matching environment variable and
+ * puts it into the object
+ */
+
+/// <reference path="../typings/tsd.d.ts" />
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+import * as _ from "lodash";
+
+
+/* Files */
+import {coerce} from "./coerce";
+
+
+export function replaceEnvVars (obj: any) {
+
+    _.each(obj, (envVar, key) => {
+
+        if (_.isPlainObject(envVar)) {
+            replaceEnvVars(envVar);
+        } else {
+
+            if (_.has(process.env, envVar)) {
+
+                /* Replace the value with the envVar */
+                let tmp = process.env[envVar];
+
+                obj[key] = coerce(tmp);
+
+            } else {
+                /* No known envvar - delete it */
+                delete obj[key];
+            }
+
+        }
+
+    });
+
+    return obj;
+
+}
---
+++ test/unit/helpers/replaceEnvVars.test.ts
@@ -0,0 +1,63 @@
+/**
+ * replaceEnvVars.test
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+import {expect, sinon} from "../../helpers/configure";
+import {replaceEnvVars} from "../../../helpers/replaceEnvVars";
+
+
+describe("replaceEnvVars test", function () {
+
+    it("should ignore if no environment variables set", function () {
+
+        var obj = {
+            envvar1: "ENVVAR1",
+            child: {
+                envvar2: "ENVVAR2"
+            }
+        };
+
+        expect(replaceEnvVars(obj)).to.be.eql({
+            child: {}
+        });
+
+    });
+
+    it("should replace with the environment variable if present", function () {
+
+        var stub = sinon.sandbox.create().stub(process, "env", {
+            "ENVVAR1": "var1",
+            "ENVVAR2": "var2",
+            "ENVVAR3": "var3"
+        });
+
+        var obj = {
+            envvar1: "ENVVAR1",
+            envvar4: "ENVVAR4",
+            child: {
+                envvar2: "ENVVAR2"
+            }
+        };
+
+        expect(replaceEnvVars(obj)).to.be.eql({
+            envvar1: "var1",
+            child: {
+                envvar2: "var2"
+            }
+        });
+
+        stub.restore();
+
+    });
+
+});
