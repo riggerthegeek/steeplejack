6e54930d9e01f74e791468270e541f63ed50881c
Simon Emms <simon@simonemms.com>
2016-01-12 11:27:16 +0000

    String coercion helper written. Converts a string into a number, bool or null type

---
+++ helpers/coerce.ts
@@ -0,0 +1,52 @@
+/**
+ * Coerce
+
+ * Coerces the value into it's datatype
+ */
+
+/// <reference path="../typings/tsd.d.ts" />
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+
+
+const IS_NUMBER = /^(\-)?(\d+(\.\d+)?)$/;
+
+
+export function coerce (value: string) : any {
+
+    let coercedValue: any = value;
+
+    if (value.match(IS_NUMBER)) {
+        coercedValue = Number(value);
+    } else {
+
+        switch (value) {
+
+            case "true":
+                coercedValue = true;
+                break;
+
+            case "false":
+                coercedValue = false;
+                break;
+
+            case "null":
+                coercedValue = null;
+                break;
+
+        }
+
+    }
+
+    return coercedValue;
+
+}
