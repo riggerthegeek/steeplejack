419aef3b9989399fd325386ff13f78581ec8011b
Simon Emms <simon@simonemms.com>
2016-01-14 13:08:17 +0000

    Added typings for the validation

--- typings/datautils/datautils.d.ts
+++ typings/datautils/datautils.d.ts
@@ -13,4 +13,18 @@ declare module "datautils" {
         static setRegex(regex: any, input: any, def?: any) : any;
         static setString(input:any, def?:any, values?:any):number;
     }
+    export class validation {
+        static email(value: string) : boolean;
+        static equal(value: any, match: any) : boolean;
+        static greaterThan(value: any, target: any) : boolean;
+        static greaterThanOrEqual(value: any, target: any) : boolean;
+        static length(value: any, length: number) : boolean;
+        static lengthBetween(value: any, minLength: number, maxLength: number) : boolean;
+        static lessThan(value: any, target: any) : boolean;
+        static lessThanOrEqual(value: any, target: any) : boolean;
+        static maxLength(value: any, length: number) : boolean;
+        static minLength(value: any, length: number) : boolean;
+        static regex(value: string, length: number) : boolean;
+        static required(value: any) : boolean;
+    }
 }
