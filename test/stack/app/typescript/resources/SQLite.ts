fa6ac5ab58a9b5a42af7df1bc9f59ab4e60fede2
Simon Emms <simon@slashdevslashnull.it>
2016-04-27 09:34:20 +0100

    The file-based JSON db in typescript

--- test/stack/app/typescript/resources/SQLite.ts
+++ test/stack/app/typescript/resources/SQLite.ts
@@ -6,19 +6,82 @@
 
 
 /* Node modules */
+import * as fs from "fs";
 import * as path from "path";
 
 
 /* Third-party modules */
+import * as _ from "lodash";
 let Bluebird = require("bluebird");
 let Pool = require("generic-pool").Pool;
-let sqlite3 = require("sqlite3");
 
 
 /* Files */
 
 
-let SQLite3 = sqlite3.verbose();
+class SQLite3 {
+
+    protected _filename: string;
+    protected _data: any;
+
+    constructor (filename: string) {
+        this._filename = filename;
+        this._data = require(this._filename);
+    }
+
+    close () { }
+
+    get (table: string, match: Object, limit: number) {
+
+        return Bluebird.try(() => {
+
+            if (_.has(this._data, table)) {
+
+                let data: any = this._data[table];
+
+                data = _.filter(data, match);
+
+                if (_.isNumber(limit) && limit > 0) {
+                    data = _.slice(data, 0, limit);
+                }
+
+                return data;
+
+            }
+
+            return [];
+
+        });
+
+    }
+
+    insert (table: string, input: any) {
+
+        return Bluebird.try(() => {
+
+            if (_.has(this._data, table) === false) {
+                this._data[table] = [];
+            }
+
+            let data: any = this._data[table];
+
+            let last: any = _.last(data);
+
+            input.id = last ? String(Number(last.id) + 1) : 1;
+
+            this._data[table].push(input);
+
+            fs.writeFileSync(this._filename, JSON.stringify(this._data), "utf8");
+
+            this._data = require(this._filename);
+
+            return input;
+
+        });
+
+    }
+
+}
 
 
 /* Defines the public output */
@@ -30,9 +93,9 @@ exports.__factory = {
             name: "sqlite",
             create: (cb: (err: any, result: any) => void) => {
 
-                var db = new SQLite3.Database(path.join(__dirname, $config.sqlite.filename));
+                let db = new SQLite3(path.join(__dirname, $config.sqlite.filename));
 
-                cb(null, Bluebird.promisifyAll(db));
+                cb(null, db);
 
             },
             destroy: (client: any) => {
