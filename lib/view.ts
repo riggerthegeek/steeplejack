0e4638dafa41f2f7034ac029e4828a07fb2e3d6e
Simon <simon@simonemms.com>
2016-06-02 15:35:51 +0100

    Added the view, which is to wrap the output so it can be automatically parsed to a template

---
+++ lib/view.ts
@@ -0,0 +1,79 @@
+/**
+ * view
+ */
+
+/// <reference path="../typings/index.d.ts" />
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+
+
+export class View {
+
+
+    protected _data: any = {};
+
+
+    protected _template: string = null;
+
+
+    public constructor ({ template, data }: { template: string, data: any }) {
+
+        this._template = template;
+        this._data = data;
+
+    }
+
+
+    /**
+     * Get Render Data
+     *
+     * Gets the render data
+     *
+     * @returns {*}
+     */
+    public getRenderData () : any {
+        return this._data;
+    }
+
+
+    /**
+     * Get Render Template
+     *
+     * Gets the template to render to
+     *
+     * @returns {string}
+     */
+    public getRenderTemplate () : any {
+        return this._template;
+    }
+
+
+    /**
+     * Render
+     *
+     * Factory to create a View object
+     *
+     * @param {string} template
+     * @param {*} data
+     * @returns {View}
+     */
+    public static render (template: string, data: any) {
+
+        return new View({
+            template,
+            data
+        });
+
+    }
+
+
+}
---
+++ test/unit/lib/view.test.ts
@@ -0,0 +1,70 @@
+/**
+ * view.test
+ */
+
+/// <reference path="../../../typings/index.d.ts" />
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+import {expect} from "../../helpers/configure";
+import {View} from "../../../lib/view";
+
+
+describe("view test", function () {
+
+    describe("methods", function () {
+
+        describe("#constructor", function () {
+
+            it("should the template and data to the class", function () {
+
+                const obj = new View({
+                    template: "some template",
+                    data: {
+                        hello: "world"
+                    }
+                });
+
+                expect(obj.getRenderTemplate()).to.be.equal("some template");
+                expect(obj.getRenderData()).to.be.eql({
+                    hello: "world"
+                });
+
+            });
+
+        });
+
+    });
+
+    describe("static methods", function () {
+
+        describe("#render", function () {
+
+            it("should return a new instance of the view with the template and data set", function () {
+
+                const obj = View.render("a template", {
+                    data: "value"
+                });
+
+                expect(obj).to.be.instanceof(View);
+
+                expect(obj.getRenderTemplate()).to.be.equal("a template");
+                expect(obj.getRenderData()).to.be.eql({
+                    data: "value"
+                });
+
+            });
+
+        });
+
+    });
+
+});
