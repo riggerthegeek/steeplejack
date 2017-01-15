7c2ac8cbce8166c3d5b037b4b27662213e47c1fb
Simon Emms <simon@simonemms.com>
2017-01-15 12:34:35 +0000

    Created a noddy test

---
+++ test/unit/index.test.js
@@ -0,0 +1,18 @@
+/**
+ * index.test
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+const { expect } = require('../helpers/configure');
+
+describe('Index tests', () => {
+  
+  it('should', () => {
+    
+  });
+  
+});
