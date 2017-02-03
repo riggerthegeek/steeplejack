3af0e07652831329ce5a8ea1dc79c2c0c37374c9
Simon Emms <simon@simonemms.com>
2017-02-03 07:51:22 +0000

    Unit tests for the helper files

---
+++ test/unit/helpers/cliParameters.test.js
@@ -0,0 +1,186 @@
+/**
+ * cliParameters.test
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+import {expect} from "../../helpers/configure";
+import cliParameters from "../../../src/helpers/cliParameters";
+
+describe("CLI Parameters test", function () {
+
+    it("should receive no arguments", function () {
+
+        var input = [
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({});
+    });
+
+    it("should receive some string arguments", function () {
+
+        var input = [
+            "this is a string",
+            "another string"
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            "this is a string": true,
+            "another string": true
+        });
+
+    });
+
+    it("should receive arguments with values", function () {
+
+        var input = [
+            "boolT=true",
+            "boolF= false",
+            "null = null",
+            "int=235643",
+            "float=2.543",
+            "negInt=-235643",
+            "negFloat=-2.543",
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            boolT: true,
+            boolF: false,
+            null: null,
+            int: 235643,
+            float: 2.543,
+            negInt: -235643,
+            negFloat: -2.543
+        });
+
+    });
+
+    it("should parse a string to an object", function () {
+
+        var input = [
+            "obj.boolF= false"
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            obj: {
+                boolF: false
+            }
+        });
+
+    });
+
+    it("should parse a string to an object and strings", function () {
+
+        var input = [
+            "obj.boolF= false",
+            "obj.negInt=-235643"
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            obj: {
+                boolF: false,
+                negInt: -235643
+            }
+        });
+
+    });
+
+    it("should parse a string to an object and strings", function () {
+
+        var input = [
+            "boolT=true",
+            "obj.boolF= false",
+            "null = null",
+            "int=235643",
+            "float=2.543",
+            "obj.negInt=-235643",
+            "negFloat=-2.543"
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            boolT: true,
+            obj: {
+                boolF: false,
+                negInt: -235643
+            },
+            null: null,
+            int: 235643,
+            float: 2.543,
+            negFloat: -2.543
+        });
+
+    });
+
+    it("should parse a string to a multilayered object", function () {
+
+        var input = [
+            "boolT=true",
+            "obj.boolF= false",
+            "obj2.obj.null = null",
+            "obj2.obj.int=235643",
+            "obj2.obj2.float=2.543",
+            "obj.negInt=-235643",
+            "negFloat=-2.543"
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+            boolT: true,
+            obj: {
+                boolF: false,
+                negInt: -235643
+            },
+            obj2: {
+                obj: {
+                    null: null,
+                    int: 235643
+                },
+                obj2: {
+                    float: 2.543
+                }
+            },
+            negFloat: -2.543
+        });
+
+    });
+
+    it("should ignore non-strings", function () {
+
+        var input = [
+            [],
+            {},
+            new Date()
+        ];
+
+        var args = cliParameters(...input);
+
+        expect(args).to.be.an("object");
+        expect(args).to.be.eql({
+        });
+
+    });
+
+});
---
+++ test/unit/helpers/coerce.test.js
@@ -0,0 +1,73 @@
+/**
+ * coerce.test
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+import {expect} from "../../helpers/configure";
+import coerce from "../../../src/helpers/coerce";
+
+describe("coerce test", function () {
+
+    this.timeout(5000);
+
+    it("should typecast integers", function () {
+
+        for (var i = 1; i <= 100000; i++) {
+            expect(coerce(String(i))).to.be.equal(i);
+        }
+
+    });
+
+    it("should typecast negative integers", function () {
+
+        for (var i = -1; i >= -100000; i--) {
+            expect(coerce(String(i))).to.be.equal(i);
+        }
+
+    });
+
+    it("should typecast zero", function () {
+        expect(coerce("0")).to.be.equal(0);
+    });
+
+    it("should typecast floats", function () {
+
+        expect(coerce("0.03")).to.be.equal(.03);
+        expect(coerce("1.03")).to.be.equal(1.03);
+        expect(coerce("3.623438")).to.be.equal(3.623438);
+
+    });
+
+    it("should typecast negative floats", function () {
+
+        expect(coerce("-2.405301")).to.be.equal(-2.405301);
+
+    });
+
+    it("should typecast a boolean", function () {
+
+        expect(coerce("true")).to.be.true;
+        expect(coerce("false")).to.be.false;
+
+    });
+
+    it("should typecast a null", function () {
+
+        expect(coerce("null")).to.be.null;
+
+    });
+
+    it("should put everything else as a string", function () {
+
+        expect(coerce("tru")).to.be.equal("tru");
+        expect(coerce("t")).to.be.equal("t");
+        expect(coerce("f")).to.be.equal("f");
+        expect(coerce("1.203980493454f")).to.be.equal("1.203980493454f");
+
+    });
+
+});
