234140ced72a0c67afc67674b833380853006199
Simon Emms <simon@simonemms.com>
2017-02-04 22:33:08 +0000

    Pulled through the plugin

---
+++ src/lib/plugin.js
@@ -0,0 +1,61 @@
+/**
+ * Plugin
+ *
+ * Manages the registration and use of a steeplejack
+ * plugin.  This is so that whole sections of code,
+ * written in steeplejack-friendly syntax, can be
+ * exported as a separate package and reused.
+ *
+ * Isn't DRY code marvellous?
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+import {_} from "lodash";
+import { Base } from '@steeplejack/core';
+
+/* Files */
+
+export default class Plugin extends Base {
+
+   constructor (files = null) {
+        super();
+
+        this.myModules = [];
+
+        /* Set the module files */
+        this.modules = files;
+    }
+
+    /**
+     * Modules
+     *
+     * Gets the modules array
+     *
+     * @returns {*[]}
+     */
+    get modules () {
+        return this.myModules;
+    }
+
+    /**
+     * Modules
+     *
+     * Sets the modules to be included with this
+     * plugin
+     *
+     * @param {*} module
+     */
+    set modules (module) {
+        if (_.isArray(module)) {
+            /* Array of modules - cycle through */
+            _.each(module, mod => {
+                this.modules = mod;
+            });
+        } else if (_.isUndefined(module) === false && _.isNull(module) === false) {
+            this.myModules.push(module);
+        }
+    }
+
+}
---
+++ test/unit/lib/plugin.test.js
@@ -0,0 +1,75 @@
+/**
+ * Plugin.test
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+import { Base } from '@steeplejack/core';
+
+/* Files */
+import {expect} from "../../helpers/configure";
+import Plugin from "../../../src/lib/plugin";
+
+describe("Plugin test", function () {
+
+    describe("Methods", function () {
+
+        describe("#constructor", function () {
+
+            it("should extend the Base method and set no plugins by default", function () {
+
+                let obj = new Plugin();
+
+                expect(obj).to.be.instanceof(Plugin)
+                    .to.be.instanceof(Base);
+
+                expect(obj.modules).to.be.eql([]);
+
+            });
+
+            it("should set a single file string on instantiation", function () {
+
+                let obj = new Plugin("singlefile");
+
+                expect(obj.modules).to.be.eql([
+                    "singlefile"
+                ]);
+
+            });
+
+            it("should set a single function on instantiation", () => {
+
+                let func = () => { };
+
+                let obj = new Plugin(func);
+
+                expect(obj.modules).to.be.eql([
+                    func
+                ]);
+
+            });
+
+            it("should set an array of files on instantiation", () => {
+
+                let func = () => { };
+
+                let obj = new Plugin([
+                    "singlefile2",
+                    func,
+                    null,
+                    undefined
+                ]);
+
+                expect(obj.modules).to.be.eql([
+                    "singlefile2",
+                    func
+                ]);
+
+            });
+
+        });
+
+    });
+
+});
