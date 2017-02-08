533872f27cbdd695256d5bff542c43a373b71129
Simon Emms <simon@simonemms.com>
2017-02-08 07:11:43 +0000

    Added all the log methods to the babel e2e logger

--- test/e2e/babel/lib/logger.js
+++ test/e2e/babel/lib/logger.js
@@ -8,12 +8,25 @@
 
 /* Files */
 
-export default () => ({
-  info: (...args) => {
-    // eslint-disable-next-line no-console
-    console.log(`[${new Date().toISOString()}] INFO:`, ...args);
-  },
-});
+export default () => {
+  const types = [
+    'fatal',
+    'error',
+    'warn',
+    'info',
+    'debug',
+    'trace',
+  ];
+
+  return types.reduce((result, type) => {
+    result[type] = (message, data, ...args) => {
+      // eslint-disable-next-line no-console
+      console.log(`[${new Date().toISOString()}] ${type.toUpperCase()}:`, data, message, ...args);
+    };
+
+    return result;
+  }, {});
+};
 
 export const inject = {
   name: '$logger',
